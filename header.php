1c12de22a086b79abf8bfddc6adf098b030ff06b
월횽 <46058116+wolhyong@users.noreply.github.com>
2023-07-26 17:22:06 +0900

    header.php

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>wolhyong_gitblog</title>
+    <link rel="stylesheet" href="reset.css">
     <link rel="stylesheet" href="styles.css">
 </head>
 
