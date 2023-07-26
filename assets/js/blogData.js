0af7187d2cf4ad877518c0c206af1964f72e4dd3
월횽 <46058116+wolhyong@users.noreply.github.com>
2023-07-27 06:41:44 +0900

    blogData.js

---
+++ assets/js/blogData.js
@@ -0,0 +1,15 @@
+const blogData = [
+  {
+    title: "Sample Blog Post 1",
+    date: "2023-07-23",
+    summary: "This is a sample blog post about something interesting.",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  },
+  {
+    title: "Sample Blog Post 2",
+    date: "2023-07-24",
+    summary: "This is another sample blog post on a different topic.",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+  },
+  // Add more blog post objects as needed
+];
