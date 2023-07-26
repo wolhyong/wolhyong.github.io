c21b05eedd92272ccc514944d3dda9d0f4575444
월횽 <46058116+wolhyong@users.noreply.github.com>
2023-07-26 17:22:36 +0900

    index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+
+<section id="blog-section">
+    <article class="container">
+        <!-- 블로그 포스트들은 여기에 동적으로 추가됩니다 -->
+    </article>
+</section>
+
+<?php include 'footer.php'; ?>
