1256b1c7aabcfd0a76fb9901ff1d4aafe7c08c33
월횽 <46058116+wolhyong@users.noreply.github.com>
2023-07-27 05:24:02 +0900

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php include 'header.php'; ?>
-
-<section id="blog-section">
-    <article class="container">
-        <!-- 블로그 포스트들은 여기에 동적으로 추가됩니다 -->
-    </article>
-</section>
-
-<?php include 'footer.php'; ?>
