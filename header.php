69c7b8020958e9ecc80d6932aaf4518d4b1ce8d4
월횽 <46058116+wolhyong@users.noreply.github.com>
2023-07-26 17:20:28 +0900

    header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wolhyong_gitblog</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header id="header_wrap">
+        <!-- 헤더 내용 -->
+    </header>
