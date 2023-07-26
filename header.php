236d093f464243c0b4925c480a5ef2418110160d
월횽 <46058116+wolhyong@users.noreply.github.com>
2023-07-26 18:45:27 +0900

    header.php

--- header.php
+++ header.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>wolhyong_gitblog</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 </head>
 
 <body>
